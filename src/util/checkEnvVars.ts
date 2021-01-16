bbd3fc5eccb35c32d5a87f8d33c260fc4c8d2345
Eddie Englund <properllerbucher@gmail.com>
2021-01-16 22:19:34 +0100

    created a check for env variables

---
+++ src/util/checkEnvVars.ts
@@ -0,0 +1,10 @@
+import { MissingEnvError } from "./envError";
+import { envFilter as filter } from "../config/config";
+
+const checkEnvVars = async (): Promise<void> => {
+  filter.forEach((key) => {
+    if (!process.env.hasOwnProperty(key)) throw new MissingEnvError(`Missing environment variable: ${key}`);
+  });
+};
+
+export { checkEnvVars };
