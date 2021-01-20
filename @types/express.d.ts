07ddb65f4381206ffeddba0b242ca2fcc8c4cc17
Eddie Englund <properllerbucher@gmail.com>
2021-01-20 22:44:03 +0100

    types

--- @types/express.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, Express } from "express";
-import { Session } from "express-session";
-import * as cookieParser from "cookie-parser";
-
-export interface ctx {
-  req: Request & { session: Session; cookieParser };
-  res: Response;
-}
