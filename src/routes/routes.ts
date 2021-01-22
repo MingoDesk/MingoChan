ae8b66d93e742d09ce7c06cde85798d14d238365
Eddie Englund <properllerbucher@gmail.com>
2021-01-22 16:33:54 +0100

    Finished delete user (self-delete)

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,9 +2,10 @@ import { Application } from "express";
 import clientRegisterRouter from "./user/clientRegister";
 import userLoginRouter from "./user/login";
 import userLogoutRouter from "./user/logout";
+import userDeleteRouter from "./user/delete";
 
 const setupRoutes = async (app: Application): Promise<void> => {
-  app.use("/api/users", clientRegisterRouter, userLoginRouter, userLogoutRouter);
+  app.use("/api/users", clientRegisterRouter, userLoginRouter, userLogoutRouter, userDeleteRouter);
   //app.use("/api/threads");
 };
 
--- src/routes/user/delete.ts
+++ src/routes/user/delete.ts
@@ -1,19 +1,19 @@
 import { ReqCtx } from "../../@types/res";
 import { Router, Response } from "express";
-import { verifyUser } from "middleware/verifyUser";
+import { verifyUser } from "../../middleware/verifyUser";
 import { getDB } from "../../database/db";
 import { ObjectId } from "mongodb";
 
 const router = Router();
 
 router.delete("/delete", verifyUser, async (req: ReqCtx, res: Response) => {
-  //@ts-ignore
-  const deleteUser = await getDB().users.findOneAndDelete({ _id: ObjectId(req.session.user._id) });
+  const deleteUser = await getDB().users.findOneAndDelete({
+    _id: new ObjectId(req.session.user._id),
+  });
   if (deleteUser.ok !== 1)
     return res
       .status(500)
       .send({ error: "Failed to delete user", msg: "Failed due to bad user information/session" });
-
   req.session.destroy((err) => {
     if (err) {
       return res
