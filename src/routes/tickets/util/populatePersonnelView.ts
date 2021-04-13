262214ae36807a92a8aaef7b5b19302e6dfdf763
Eddie Englund <properllerbucher@gmail.com>
2021-04-13 19:47:09 +0200

    Function that populates data for the FE

---
+++ src/routes/tickets/util/populatePersonnelView.ts
@@ -0,0 +1,15 @@
+import { IPersonnelView, INoteData } from "../controllers/ticketController";
+
+const populateNotes = (notes: INoteData[], messages: IPersonnelView[]) => {
+  messages.forEach((message, index) => {
+    for (let note of notes) {
+      if (note.id != message.id) continue;
+      messages[index] = { ...note };
+      return;
+    }
+  });
+
+  return messages;
+};
+
+export { populateNotes };
