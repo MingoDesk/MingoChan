61cb4a8707e278d83b30416a1ec7226a7230c70c
Eddie Englund <properllerbucher@gmail.com>
2021-04-27 22:29:06 +0200

    Removed all tests as they're bad

--- tests/getDate.js
+++
@@ -1,4 +0,0 @@
-// This test was written to confirm if new Date() always returns UTC
-const date = new Date();
-
-console.log(date);
--- tests/populatePersonnelViewTest.js
+++
@@ -1,4 +0,0 @@
-const data = require("./data.json");
-const { populateNotes } = require("../dist/routes/tickets/util/populatePersonnelView");
-
-console.log(populateNotes(data.data.notes, data.data.personnelView));
