604309d700da80d7e7b955e9dd02205f68c7e72c
Eddie Englund <properllerbucher@gmail.com>
2021-05-02 00:09:39 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { checkEnvVars } from "./util/checkEnvVars";
 import express from "express";
 import { setupRoutes } from "./routes/routes";
 import { initializeAuth } from "./lib/setup.passport";
+import cors from "cors";
 
 const init = async (app: Application): Promise<void> => {
   // Check that all env variables are set
@@ -17,6 +18,13 @@ const init = async (app: Application): Promise<void> => {
   // Application configuration
   app.use(express.json({ limit: "100kb", strict: true, type: "application/json" }));
   app.use(helmet());
+  app.use(
+    cors({
+      origin: "http://localhost:8080",
+      optionsSuccessStatus: 200,
+      credentials: true,
+    })
+  );
   initializeAuth(app);
 
   await setupRoutes(app);
