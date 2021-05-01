120c243977e7549f6a619e61ac418ac2b1512518
Eddie Englund <properllerbucher@gmail.com>
2021-05-02 00:09:55 +0200

    Simplified logout

--- src/routes/auth/controllers/logout.ts
+++ src/routes/auth/controllers/logout.ts
@@ -5,24 +5,8 @@ import { URL } from "url";
 const router = Router();
 
 router.get("/logout", (req: Request, res: Response) => {
-  req.logOut();
-
-  let returnTo = req.protocol + "://" + req.hostname;
-  const port = req.socket.localPort;
-
-  if (port !== undefined && port !== 80 && port !== 443) {
-    returnTo = process.env.NODE_ENV === "production" ? `${returnTo}/` : `${returnTo}:${port}/`;
-  }
-
-  const logoutURL = new URL(`https://${process.env.ISSUERBASEURL}/v2/logout`);
-
-  const searchString = queryString.stringify({
-    client_id: process.env.CLIENTID,
-    returnTo: returnTo,
-  });
-  logoutURL.search = searchString;
-
-  res.redirect(logoutURL.toString());
+  req.logout();
+  res.redirect(process.env.BASE_REDIRECT_URL);
 });
 
 export default router;
