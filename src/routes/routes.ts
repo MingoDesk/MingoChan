01c92c862c272e252753536c3e3387d8bd328f0d
Eddie Englund <properllerbucher@gmail.com>
2021-05-02 17:47:54 +0200

    removed unused code

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,20 +1,11 @@
 import { Application } from "express";
-import { Request, Router } from "express";
 import callbackRouter from "./auth/controllers/callback";
 import loginRouter from "./auth/controllers/login";
 import logoutRouter from "./auth/controllers/logout";
 import ticketsRouter from "./tickets/ticketConfig";
 import systemRouter from "./system/systemConfig";
 
-const router = Router();
-
 const setupRoutes = (app: Application): void => {
-  app.use(
-    router.get("/", (req: Request, res) => {
-      return res.send({ isLoggedIn: req.user ? true : false });
-    })
-  );
-
   app.use("/api", loginRouter, callbackRouter, logoutRouter);
   app.use("/api/tickets", ticketsRouter);
   app.use("/api/system", systemRouter);
