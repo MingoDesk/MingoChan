cf8211a9a3d55e556fe9bdccf620d405b8cb587f
Eddie Englund <properllerbucher@gmail.com>
2021-05-25 23:58:02 +0200

    get systemConfig no longer requires the user to be logged in

--- src/routes/system/systemConfig.ts
+++ src/routes/system/systemConfig.ts
@@ -1,13 +1,13 @@
-import { Router } from "express";
-import { updateSystemSettingsRoute } from "./controllers/updateSystemSettings";
-import { getSystemSettings } from "./controllers/getSystemSettings";
-import { validate } from "./controllers/systemsController";
-import { secured as validateSession } from "../../middleware/validateSession";
+import { Router } from 'express';
+import { updateSystemSettingsRoute } from './controllers/updateSystemSettings';
+import { getSystemSettings } from './controllers/getSystemSettings';
+import { validate } from './controllers/systemsController';
+import { secured as validateSession } from '../../middleware/validateSession';
 
 const router = Router();
 
-router.get("/", validateSession, getSystemSettings);
+router.get('/', getSystemSettings);
 //@ts-ignore
-router.patch("/update", validateSession, validate("update"), updateSystemSettingsRoute);
+router.patch('/update', validateSession, validate('update'), updateSystemSettingsRoute);
 
 export default router;
