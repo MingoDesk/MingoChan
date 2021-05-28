acd51e4c52830876591170fc928cf951aab7f36a
Eddie Englund <properllerbucher@gmail.com>
2021-05-28 22:37:09 +0200

    Minor fixes

--- src/routes/tickets/controllers/createTicket.ts
+++ src/routes/tickets/controllers/createTicket.ts
@@ -1,19 +1,17 @@
-import { getDB } from "../../../database/db";
-import { validationResult } from "express-validator";
-import { matchedData } from "express-validator";
-import { ITicket } from "./ticketController";
-import { v4 as uuid } from "uuid";
+import { getDB } from '../../../database/db';
+import { matchedData, validationResult } from 'express-validator';
+import { ITicket } from './ticketController';
+import { v4 as uuid } from 'uuid';
+import { TicketStatus } from './ticketController';
 
 // TODO: Make sure to incorporate diffirent callbacks depending on user permissions
-
-// TODO: Add userinfo such as createdBy/author and user icon from session data instead of from the request body (This can only be done one auth is implemented again) and Add more userdata to each message
 
 const createTicket = async (req, res): Promise<ITicket> => {
   const errors = validationResult(req);
   const data = matchedData(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).send({ success: false, msg: "Bad request", errors: errors.array() });
+    return res.status(400).send({ success: false, msg: 'Bad request', errors: errors.array() });
   }
 
   const createdAt = new Date();
@@ -22,15 +20,22 @@ const createTicket = async (req, res): Promise<ITicket> => {
   const newTicket = await getDB().tickets.insertOne({
     authorId: req.user.sub,
     author: req.user.name,
-    assignee: null,
-    createdAt,
+    status: TicketStatus.open,
+    createdAt: createdAt,
     // TODO: Check if customer group is marked as "starred"
     isStarred: false,
     tags: [],
     labels: [],
-    rating: null,
     isUpdated: true,
-    messages: [{ ...data, author: req.user.name, authorId: req.user.sub, createdAt, id: ticketId }],
+    messages: [
+      {
+        text: data.text,
+        author: req.user.name,
+        authorId: req.user.sub,
+        createdAt,
+        id: ticketId,
+      },
+    ],
     notes: [],
     personnelView: [{ id: ticketId }],
   });
@@ -38,8 +43,8 @@ const createTicket = async (req, res): Promise<ITicket> => {
   if (!newTicket) {
     return res.status(500).send({
       success: false,
-      msg: "Something went wrong when saving to DB, please try again",
-      error: "Internal server error",
+      msg: 'Something went wrong when saving to DB, please try again',
+      error: 'Internal server error',
     });
   }
 
