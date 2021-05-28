0aed290e18777992dbb0bfeef6393e5ce6ca938a
Eddie Englund <properllerbucher@gmail.com>
2021-05-28 22:36:29 +0200

    lint

--- src/routes/exports/exportConfig.ts
+++ src/routes/exports/exportConfig.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
-import { exportFile } from "./controllers/exportController";
-import { secured as validateSession } from "../../middleware/validateSession";
+import { Router } from 'express';
+import { exportFile } from './controllers/exportController';
+import { secured as validateSession } from '../../middleware/validateSession';
+
 const router = Router();
 
-router.get("/", validateSession, exportFile);
-//@ts-ignore
+router.get('/', validateSession, exportFile);
 
 export default router;
