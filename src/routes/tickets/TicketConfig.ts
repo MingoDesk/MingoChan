3053b11f5d126c36bb1d748bb7b09e2d92e5958f
Eddie Englund <properllerbucher@gmail.com>
2021-05-28 22:37:19 +0200

    get assigned feed

--- src/routes/tickets/TicketConfig.ts
+++ src/routes/tickets/TicketConfig.ts
@@ -3,6 +3,7 @@ import { createTicket } from './controllers/createTicket';
 import { getTicket } from './controllers/getTicket';
 import { replyTicket } from './controllers/replyTicket';
 import { getUnassignedTickets } from './controllers/getUnassignedTickets';
+import { getAssignedTickets } from './controllers/getAssignedTickets';
 import { validate } from './controllers/ticketController';
 import { secured as validateSession } from '../../middleware/validateSession';
 import noteRouter from './notes/noteConfig';
@@ -15,6 +16,7 @@ router.post('/new', validateSession, validate('createTicket'), createTicket);
 router.patch('/reply', validateSession, validate('replyTicket'), replyTicket);
 router.get('/:id', validateSession, getTicket);
 router.get('/unassigned/feed', getUnassignedTickets);
+router.get('/assigned/feed', getAssignedTickets);
 router.use('/notes', noteRouter);
 
 export default router;
