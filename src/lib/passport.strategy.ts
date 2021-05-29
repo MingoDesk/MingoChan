d08c68edc8c0022e963cb8a0dc99a4c04ea8b587
Eddie Englund <properllerbucher@gmail.com>
2021-05-30 00:33:13 +0200

    idk?

--- src/lib/passport.strategy.ts
+++ src/lib/passport.strategy.ts
@@ -2,35 +2,35 @@ import Auth0Strategy, { ExtraVerificationParams, Profile } from 'passport-auth0'
 import { getDB } from '../database/db';
 
 const setupStrategy = () => {
-  const strategy = new Auth0Strategy(
-    {
-      domain: process.env.ISSUER_BASEURL,
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.SECRET,
-      callbackURL: `${process.env.BASEURL}/api/callback`,
-    },
-    async function (
-      accessToken: string,
-      refreshToken: string,
-      extraParams: ExtraVerificationParams,
-      profile,
-      done: any
-    ): Promise<any> {
-      // Note that these parameters will only be availbe if the rule "Add country to the user profile" is enabled on the auth0 project. If these values are null, check there.
-      const { 'https://mingochan.com/country': country, 'https://mingochan.com/timezone': timezone } = profile._json;
+	const strategy = new Auth0Strategy(
+		{
+			domain: process.env.ISSUER_BASEURL,
+			clientID: process.env.CLIENT_ID,
+			clientSecret: process.env.SECRET,
+			callbackURL: `${process.env.BASEURL}/api/callback`,
+		},
+		async function (
+			accessToken: string,
+			refreshToken: string,
+			extraParams: ExtraVerificationParams,
+			profile,
+			done: any,
+		): Promise<any> {
+			// Note that these parameters will only be availbe if the rule "Add country to the user profile" is enabled on the auth0 project. If these values are null, check there.
+			const { 'https://mingochan.com/country': country, 'https://mingochan.com/timezone': timezone } = profile._json;
 
-      getDB().users.findOneAndUpdate(
-        { _id: profile.id },
-        { $set: { ...profile._json, country, timezone } },
-        { upsert: true, returnOriginal: false },
-        (err, res) => {
-          if (err) return done(err);
-          return done(null, res.value);
-        }
-      );
-    }
-  );
+			getDB().users.findOneAndUpdate(
+				{ _id: profile.id },
+				{ $set: { ...profile._json, country, timezone } },
+				{ upsert: true, returnOriginal: false },
+				(err, res) => {
+					if (err) return done(err);
+					return done(null, res.value);
+				},
+			);
+		},
+	);
 
-  return strategy;
+	return strategy;
 };
 export { setupStrategy };
