7b834c2cf70c8cad84a25034392f7c25620b2135
Eddie Englund <properllerbucher@gmail.com>
2021-05-30 00:34:53 +0200

    Fixed pagination bug

--- src/routes/tickets/controllers/getAssignedTickets.ts
+++ src/routes/tickets/controllers/getAssignedTickets.ts
@@ -1,32 +1,33 @@
 import { getDB } from '../../../database/db';
 import { TicketStatus } from './ticketController';
-import { find } from 'mongo-cursor-pagination';
+import { find } from '@tadashi/mongo-cursor-pagination';
+import { getMetadataFromTicket } from '../util/getMetadataFromTicket';
 
 const getAssignedTickets = async (req, res) => {
-  if (!req.query.userId) {
-    return res.status(400).send({ success: false, error: 'Bad request', msg: 'You must provide a userId!' });
-  }
+	if (!req.query.userId) {
+		return res.status(400).send({ success: false, error: 'Bad request', msg: 'You must provide a userId!' });
+	}
 
-  const tickets = await find(getDB().tickets, {
-    limit: 50,
-    fields: { assignee: req.query.userId, status: TicketStatus.open },
-    next: req.query.hasNext == 'true' ? true : false,
-  });
+	const tickets = await find(getDB().tickets, {
+		limit: 50,
+		query: { assignee: req.query.userId, status: TicketStatus.open },
+		next: req.query.hasNext == 'true' ? true : false,
+	});
 
-  if (!Array.isArray(tickets.results) || !tickets.results.length) {
-    return res.status(200).send({
-      sucess: false,
-      error: 'No tickets found',
-      msg: "You don't have any tickets!",
-    });
-  }
-  const data = tickets.results.map(({ rating, messages, personnelView, notes, ...metaData }) => metaData);
+	if (!Array.isArray(tickets.results) || !tickets.results.length) {
+		return res.status(200).send({
+			sucess: false,
+			error: 'No tickets found',
+			msg: "You don't have any tickets!",
+		});
+	}
+	const data = getMetadataFromTicket(tickets);
 
-  return res.status(200).send({
-    success: true,
-    errors: null,
-    data,
-  });
+	return res.status(200).send({
+		success: true,
+		errors: null,
+		data,
+	});
 };
 
 export { getAssignedTickets };
--- src/routes/tickets/controllers/getUnassignedTickets.ts
+++ src/routes/tickets/controllers/getUnassignedTickets.ts
@@ -1,29 +1,27 @@
 import { getDB } from '../../../database/db';
-import { find } from 'mongo-cursor-pagination';
+import { find } from '@tadashi/mongo-cursor-pagination';
 import { TicketStatus, ITicket } from './ticketController';
 import { IPaginateResult } from '../../../@types/paginate';
 
 const getUnassignedTickets = async (req, res) => {
-  console.log(req.query.hasNext == 'true' ? true : false);
-  const tickets: IPaginateResult<ITicket> = await find(getDB().tickets, 'assignee', {
-    limit: 50,
-    fields: { _id: 1, assignee: 'github|16852656', status: TicketStatus.open },
-    next: req.query.hasNext == 'true' ? true : false,
-  });
+	// Get all tickets that aren't assigned to anyone and that has an open status
 
-  if (!Array.isArray(tickets.results) || !tickets.results.length)
-    return res.status(200).send({ sucess: false, error: 'No tickets found', msg: "There aren't any tickets 🥳" });
+	const tickets: IPaginateResult<ITicket> = await find(getDB().tickets, {
+		limit: 50,
+		query: { status: TicketStatus.open, $or: [{ assignee: { $type: 'null' } }, { assignee: { $exists: false } }] },
+		next: req.query.hasNext == 'true' ? true : false,
+	});
 
-  //console.log(data);
-  const data = tickets.results.map(({ rating, messages, personnelView, notes, ...metaData }) => metaData);
+	if (!Array.isArray(tickets.results) || !tickets.results.length)
+		return res.status(200).send({ sucess: false, error: 'No tickets found', msg: "There aren't any tickets 🥳" });
 
-  console.log(data);
+	const data = tickets.results.map(({ rating, messages, personnelView, notes, ...metaData }) => metaData);
 
-  return res.status(200).send({
-    success: true,
-    errors: null,
-    data,
-  });
+	return res.status(200).send({
+		success: true,
+		errors: null,
+		data,
+	});
 };
 
 export { getUnassignedTickets };
