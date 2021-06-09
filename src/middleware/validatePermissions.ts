cb873e783425ecf5ba56dc783149d36187638620
Eddie Englund <properllerbucher@gmail.com>
2021-06-09 18:36:01 +0200

    Fixed isse where if perms don't exist, then we throw an error

--- src/middleware/validatePermissions.ts
+++ src/middleware/validatePermissions.ts
@@ -5,32 +5,62 @@ import { SysAdmin, StaffAdmin, Staff, OrgUser, User } from '../routes/user/contr
 const check = (userPerms, ref) => ref.every((v) => userPerms.includes(v));
 
 const validateSysAdminPerms = (req: Request, res: Response) => {
+	if (req.user!.permissions == undefined) {
+		return res
+			.status(400)
+			.send({ ...responseGenerator(400, "You don't have the permissions to perform this action!") });
+	}
+
 	if (!check(req.user!.permissions, SysAdmin.permissions)) {
-		return res.status(400).send({ ...responseGenerator(400) });
+		return res
+			.status(400)
+			.send({ ...responseGenerator(400, "You don't have the permissions to perform this action!") });
 	}
 };
 
 const validateStaffAdminPerms = (req: Request, res: Response) => {
+	if (req.user!.permissions == undefined) {
+		return res
+			.status(400)
+			.send({ ...responseGenerator(400, "You don't have the permissions to perform this action!") });
+	}
+
 	if (!check(req.user!.permissions, StaffAdmin.permissions)) {
-		return res.status(400).send({ ...responseGenerator(400) });
+		return res
+			.status(400)
+			.send({ ...responseGenerator(400, "You don't have the permissions to perform this action!") });
 	}
 };
 
 const validateStaffPerms = (req: Request, res: Response) => {
-	console.log('x marks the spot', req.user);
+	if (req.user!.permissions == undefined) {
+		return res
+			.status(400)
+			.send({ ...responseGenerator(400, "You don't have the permissions to perform this action!") });
+	}
 
 	if (!check(req.user!.permissions, Staff.permissions)) {
-		return res.status(400).send({ ...responseGenerator(400) });
+		return res
+			.status(400)
+			.send({ ...responseGenerator(400, "You don't have the permissions to perform this action!") });
 	}
 };
 
 const validateOrgUserPerms = (req: Request, res: Response) => {
+	if (req.user!.permissions == undefined) {
+		return res.status(400).send({ ...responseGenerator(400) });
+	}
+
 	if (!check(req.user!.permissions, OrgUser.permissions)) {
 		return res.status(400).send({ ...responseGenerator(400) });
 	}
 };
 
 const validateUserPerms = (req: Request, res: Response) => {
+	if (req.user!.permissions == undefined) {
+		return res.status(400).send({ ...responseGenerator(400) });
+	}
+
 	if (!check(req.user!.permissions, User.permissions)) {
 		return res.status(400).send({ ...responseGenerator(400) });
 	}
