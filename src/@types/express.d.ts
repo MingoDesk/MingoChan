5d2dea6a7bdedb333f770efb4cf322c4df2da23a
Eddie Englund <properllerbucher@gmail.com>
2021-06-11 07:26:43 +0200

    fixed namin conventions for interface

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -2,20 +2,20 @@ import { Profile } from 'passport-auth0';
 import { ISysAdmin, IStaff, IStaffAdmin, IOrgUser } from 'routes/user/controllers/userController';
 import express = require('express');
 
-interface sessionUser extends Profile {
+interface ISessionUser extends Profile {
 	permissions: ISysAdmin['permissions'] | IStaff['permissions'] | IStaffAdmin['permissions'] | IOrgUser['permissions'];
 }
 
 declare module 'express-session' {
 	interface session {
-		user: sessionUser;
+		user: ISessionUser;
 		returnTo: string;
 	}
 }
 
 declare global {
 	namespace Express {
-		interface User extends sessionUser {}
+		interface User extends ISessionUser {}
 		interface Request {
 			user?: User;
 		}
