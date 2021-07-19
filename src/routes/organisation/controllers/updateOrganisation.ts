fc2e599b6e115a3c6aace5312980aac31a4b482b
Eddie Englund <properllerbucher@gmail.com>
2021-07-19 20:59:16 +0200

    update org

--- src/routes/organisation/controllers/updateOrganisation.ts
+++ src/routes/organisation/controllers/updateOrganisation.ts
@@ -11,11 +11,30 @@ const updateOrganisation = async (req: Request, res: Response) => {
 		return res.status(400).send({ success: false, msg: 'Bad request', errors: errors.array() });
 	}
 
+	const users = await getDB()
+		.users.find({
+			_id: { $in: data.users },
+			systemOrganisationId: req.user!.systemOrganisationId,
+		})
+		.toArray();
+
+	if (!users.length || users.length !== data.users.length) {
+		return res.status(400).send({ ...responseGenerator(400, 'Invalid users') });
+	}
+
 	const updateData = await getDB().organisations.findOneAndUpdate(
 		{ _id: data.organisationId },
-		{},
+		{
+			$set: { users: data.users },
+		},
 		{ returnOriginal: false },
 	);
+
+	if (updateData.ok !== 1 || !updateData.value) {
+		return res.status(500).send({ ...responseGenerator(500, 'Failed to update organisation, please try again!') });
+	}
+
+	return res.status(200).send({ ...responseGenerator(200), data: { ...updateData.value } });
 };
 
 export { updateOrganisation };
