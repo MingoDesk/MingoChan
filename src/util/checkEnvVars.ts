71252c8ca300cc9138b571d4b8bceee9de52275d
Eddie Englund <properllerbucher@gmail.com>
2021-08-06 19:01:20 +0200

    No longer loggin found keys

--- src/util/checkEnvVars.ts
+++ src/util/checkEnvVars.ts
@@ -4,7 +4,6 @@ import { envFilter as filter } from '@config/config';
 const checkEnvVars = (): void => {
 	filter.forEach((key) => {
 		if (!process.env.hasOwnProperty(key)) throw new MissingEnvError(`Missing environment variable: ${key}`);
-		console.log(`Found key: ${key}`);
 	});
 };
 
