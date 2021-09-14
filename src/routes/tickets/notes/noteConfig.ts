19540a96b965cd9a683746e64ca4dd1c694d455b
Eddie Englund <properllerbucher@gmail.com>
2021-09-14 20:19:04 +0200

    imports

--- src/routes/tickets/notes/noteConfig.ts
+++ src/routes/tickets/notes/noteConfig.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
+import { secured as validateSession } from '@middleware/validateSession';
+import { validateStaffPerms, validateSysAdminPerms } from '@middleware/validatePermissions';
 import { createNote } from './controllers/createNote';
 import { editNote } from './controllers/editNote';
 import { validate } from './controllers/noteController';
-import { secured as validateSession } from '@middleware/validateSession';
-import { validateStaffPerms, validateSysAdminPerms } from '@middleware/validatePermissions';
 
 const router = Router();
 
