30f73d6e59930fb93180678191cdb5317bcec7d9
Eddie Englund <properllerbucher@gmail.com>
2021-09-14 20:18:04 +0200

    get user

--- src/routes/user/userConfig.ts
+++ src/routes/user/userConfig.ts
@@ -1,11 +1,13 @@
-import { updateUserPermissions } from './controllers/updateUserPermissions';
 import { Router } from 'express';
 import { validateSysAdminPerms } from '@middleware/validatePermissions';
 import { secured as validateSession } from '@middleware/validateSession';
+import { updateUserPermissions } from './controllers/updateUserPermissions';
 import { validate } from './controllers/userController';
+import { getUser } from './controllers/getUser';
 
 const router = Router();
 
 router.patch('/update', validateSession, validateSysAdminPerms, validate('update'), updateUserPermissions);
+router.get('/', validateSession, getUser);
 
 export default router;
