72cc87db5999f2c090488f11aa7a850709fb7d11
Eddie Englund <properllerbucher@gmail.com>
2021-10-30 21:56:03 +0200

    Email now returns proper data

--- src/routes/user/helpers/getSafeUser.ts
+++ src/routes/user/helpers/getSafeUser.ts
@@ -1,18 +1,16 @@
 import { Request } from 'express';
 
-const getSafeUser = (req: Request) => {
+export const getSafeUser = (req: Request) => {
 	const u = req.user;
 
 	if (!u) return null;
 	return {
 		permissions: u.permissions,
 		providerId: u.providerId,
-		email: u._id,
+		email: u.email,
 		isVerified: u.email,
 		locale: u.locale,
 		name: u.name,
-		picture: u.picture
+		picture: u.picture,
 	};
 };
-
-export { getSafeUser };
